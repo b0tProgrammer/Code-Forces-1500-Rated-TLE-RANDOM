39491eeae1f501d0ecfdcdc5883163e50b73e581
Mohith Kumar <155897668+b0tProgrammer@users.noreply.github.com>
2025-01-08 00:44:43 +0530

    Add files via upload

---
+++ Smilo and monsters.java
@@ -0,0 +1,300 @@
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class Main {
+
+    private static final int mod = (int) 1e9 + 7; //mod
+    private static final int iif = 998244353; // int infinity!
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+            Comparator<Pair> com = Comparator.comparingInt(i -> i.v1);
+
+            int testcases = 1;
+
+            testcases = in.nextInt();
+
+            while(testcases-- > 0) {
+                /*
+                    In this, we have 2 operations, they are
+                    1) choose i from 1 to n and kill those monsters in a horde with an attack that can kill one monster
+                    for unit cost
+                    2) else for every kill of monster we are having a counter-attack boost of 1 so it will be boosted for every kill of monster
+                    So, using these 2 operations, we have to minimize the number of operations in order to kill all the monsters
+                 */
+                int siz = in.nextInt();
+                int[] nums = in.nextIntArray(0,siz);
+                long x = 0; // counter cost
+                long ans = 0; // the main answer counts the operations
+                Arrays.sort(nums); // sorting the array such that can choose the type 1 for few monsters.
+                // And increase our counter value to decrease large number of monsters with minimum cost
+                int l = 0;
+                int r = siz-1;
+                while(l < r) {
+                    if(x+nums[l] < nums[r]) {
+                        x += nums[l];
+                        ans += nums[l];
+                        l++;
+                    } else {
+                        long extra = nums[r]-x; // calculating the required amount such that we can easily kill monsters at nums[r] for cost of 1
+                        ans += extra; // adding the cost that is needed to complete the process.
+                        ans++;
+                        nums[l] -= (int) extra; // removing the extra part from the nums[l]
+                        r--;
+                        x = 0; // resetting the counter-attack as we have used it!
+                    }
+                }
+                if(nums[l] == 1) {
+                    ans++; // if there is only one monster left in the horde then use one operation of type 1 to kill it!
+                } else if(nums[l] > 1){
+                    // first we remove the killed monsters from the current horde
+                    // then to find the number of operations, we find that using the below formula
+                    // which gives the ceil division of the remaining monsters
+                    // for example if there are 7 monsters we kill 3 of them and remove 3 using type 2 costs 4 and another unit cost to remove 1 monster
+                    ans += (nums[l]-x+1)/2; // killing half of them with operation type 1
+                    ans++; // then killing half of them with operation type 2
+                }
+                out.println(ans);
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    //<--------------------------------------Fast reader----------------------------------------->
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        public int[] nextIntArray(int start, int end) {
+            int[] nums = new int[end];
+            for(int i = start; i < end; i++) {
+                nums[i] = Integer.parseInt(next());
+            }
+            return nums;
+        }
+
+        public Pair[] nextPairArray(int siz) {
+            Pair[] nums = new Pair[siz];
+            for(int i = 0; i < siz; i++) {
+                int v1 = Integer.parseInt(next());
+                int v2 = Integer.parseInt(next());
+                nums[i] = new Pair(v1,v2);
+            }
+            return nums;
+        }
+
+        public long[] nextLongArray(int siz) {
+            long[] nums = new long[siz];
+            for(int i = 0; i < siz; i++) {
+                nums[i] = Long.parseLong(next());
+            }
+            return nums;
+        }
+
+    }
+
+//<---------------------------------------Fast writer--------------------------------------------->
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println() throws IOException {
+            bw.append("\n");
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+// <-------------------------------------------------Some helper methods---------------------------------------->
+
+    private static long kadane(int l, int r, int[] nums) {
+        long sum = 0;
+        long mSum = 0;
+        for(int i = l; i < r; i++) {
+            if(sum < 0) sum = nums[i];
+            else sum += nums[i];
+            mSum = max(sum,mSum);
+        }
+        return mSum;
+    }
+
+    public static List<Integer> reversed(List<Integer> lis) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = lis.size()-1; i >= 0; i--) {
+            ans.add(lis.get(i));
+        }
+        return ans;
+    }
+
+    private static void getFactors(int num, Map<Integer, Integer> map) {
+
+        for(int i = 2; i <= (int) Math.sqrt(num); i++) {
+            while(num%i == 0) {
+                map.put(i,map.getOrDefault(i,0)+1);
+                num /= i;
+            }
+        }
+        if(num > 1) map.put(num,map.getOrDefault(num,0)+1);
+    }
+
+    public static void print(boolean cond,FastWriter out) throws IOException {
+        out.println(cond ? "Yes" : "No");
+    }
+
+    public static long fastPow(long b, long e) {
+        long res = 1;
+        while (e > 0) {
+            if (odd(e)) res = (res * b) % mod;
+            b = (b * b) % mod;
+            e = e >> 1;
+        }
+        return res;
+    }
+
+    public static int charToInt(char c) {
+        return (c - '0');
+    }
+
+    public static char intToChar(int n) {
+        return (char) (n + 48);
+    }
+
+    public static int[] sieve(int upto) {
+        int[] primes = new int[upto + 1];
+        Arrays.fill(primes, 1);
+        for (int i = 2; i * i <= upto; i++) {
+            if (primes[i] == 1)
+                for (int j = i * i; j <= upto; j += i) {
+                    primes[j] = 0;
+                }
+        }
+        return primes;
+    }
+
+    private static long fact(int num) {
+        long v = 1;
+        for(int i = 1; i <= num; i++) {
+            v = (v*i)%mod;
+        }
+        return v;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static long getSum(int startNum, int commonDiff, long range) {
+        long lastNum = startNum + (range - 1) * commonDiff;
+        return range * (startNum + lastNum) / 2;
+    }
+
+    private static boolean odd(long siz) {
+        return (siz & 1) == 1;
+    }
+
+    private static boolean powOf2(long siz) {
+        return (siz & (siz - 1)) == 0;
+    }
+
+    private static List<Integer> primeFactors(int siz) {
+
+        List<Integer> sizs = new ArrayList<>();
+        int[] p;
+        p = sieve(siz);
+        for (int i = 2; i <= siz; i++) {
+            while (siz % i == 0 && p[i] == 1) {
+                sizs.add(i);
+                siz /= i;
+            }
+        }
+        return sizs;
+    }
+}
+
+class Pair2 {
+    long v1;
+    long v2;
+
+    public Pair2(long v1, long v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
+class Pair{
+    int v1,v2;
+    public Pair(int v,int w) {v1 = v; v2 = w;}
+}
+class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+    }
+}
